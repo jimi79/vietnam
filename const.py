96d30afe491921f623e523edb99ca8b0a2034bbd
pi <pi@lorem>
2020-02-04 15:26:20 +0100

    damn, i forgot the debug mode

--- const.py
+++ const.py
@@ -24,11 +24,11 @@ QUERY_DONE = 2 # query complete, could be executed
 QUERY_ERR = 1 # key not authorized
 QUERY_NEXT = 0 # query not complete
 
-SIZE = 10
+SIZE = 20
 CELL_RESOLUTION = 5
 
 COUNT_PLAYER_TEAMS = 2
-COUNT_NPC_TEAMS = 6
+COUNT_NPC_TEAMS = 8
 
 SPEED_FACTOR = 1 # 1 normal, 2 faster
 
@@ -41,10 +41,10 @@ RATIO_WONDER = 0.2
 
 #debug
 
-RATIO_FOREST = 0
-SIZE = 10
-RATIO_WATER = 0
-SPEED_FACTOR = 20 # 1 normal, 2 faster
-DEBUG = True
-COUNT_NPC_TEAMS = 0
+#RATIO_FOREST = 0
+#SIZE = 10
+#RATIO_WATER = 0
+#SPEED_FACTOR = 20 # 1 normal, 2 faster
+#DEBUG = True
+#COUNT_NPC_TEAMS = 0
 #SUPERMAN = False
