f4d4ee507b5720ad5c969625f02bcc2df668f47c
pi <pi@lorem>
2020-02-10 14:39:48 +0100

    bug

--- tools.py
+++ tools.py
@@ -5,7 +5,7 @@ from const import *
 def rnd():
 	return random.randrange(0, SIZE) 
 
-def log(text):
+def debug(text):
 	if DEBUG:
 		f = open("debug", "a")
 		f.write("%s: %s\n" % (datetime.datetime.now().strftime("%H:%M:%S"), text)) 
